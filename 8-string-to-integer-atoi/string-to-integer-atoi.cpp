f61946f184819c3f1091c578aff5c1520d1a7613
Salman Jan <jsalman862@gmail.com>
2024-10-29 23:24:10 +0500

    Time: 37 ms (5.43%) | Memory: 8.9 MB (97.14%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int myNum = 0;
+        int i;
+        for (i = 0; s[i] == ' '; i++) {
+            continue;
+        }
+
+        int sign = 1;
+
+        if (s[i] == '-') {
+            sign = -1;
+            i++;
+        }
+        else if(s[i] == '+'){
+            sign = 1;
+            i++;
+        }
+
+        // cout << sign<<endl;
+        while (s[i] == '0')
+            i++;
+
+        int digit = 0;
+        while (s[i] != '\0' && s[i] >= 48 && s[i] <= 57) {
+            digit = s[i] - 48;
+            cout<<"digit " << digit<<endl;
+            // cout<<digit<<endl;
+            // cout<<myNum<<endl;
+            if(myNum > 0 && sign == -1){
+                myNum *= sign;
+            }
+            cout<<myNum<<endl;
+            if (myNum > INT_MAX / 10 ) {
+                myNum = INT_MAX;
+                break;
+            } else if (myNum < INT_MIN / 10) {
+                myNum = INT_MIN;
+                break;
+            }
+            myNum *= 10;
+            if(myNum > INT_MAX - digit){
+                myNum = INT_MAX;
+                break;
+            }
+            else if(myNum < INT_MIN + digit){
+                myNum = INT_MIN;
+                break;
+            }
+            if(myNum >= 0){
+                myNum += digit;
+                cout<<"My num "<<myNum<<endl;
+            }
+            else
+                myNum -= digit;
+            i++;
+        }
+
+        if(myNum > 0 && sign == -1)
+            myNum *= sign;
+
+        return myNum;
+    }
+};
