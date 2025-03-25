9379c1a6d6016bd68d74ed239ebcf8d3a0f4c4db
Salman Jan <jsalman862@gmail.com>
2025-03-25 23:20:57 +0500

    Time: 3 ms (29.85%) | Memory: 11.9 MB (15.56%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answers;
+        for(int i=1; i<=n;i++){
+            if(i%3 ==0 && i%5 ==0)
+                answers.push_back("FizzBuzz");
+            else if(i%3 ==0)
+                answers.push_back("Fizz");
+            else if(i%5 ==0)
+                answers.push_back("Buzz");
+            else
+                answers.push_back(to_string(i));
+        }
+
+        return answers;
+    }
+};
