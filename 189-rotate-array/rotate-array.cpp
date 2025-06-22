9151594b5361f463de9d114e9b52e2bb39996e32
Salman Jan <jsalman862@gmail.com>
2025-06-23 00:13:03 +0500

    Time: 461 ms (2.72%) | Memory: 29.5 MB (57.38%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size = nums.size();
+        if(size <=1)
+            return;
+
+        k %= size;
+        int left = 0;
+        int right = size - 1;
+        int temp;
+
+        // 1- rotate the whole array
+        while (left < right) {
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < size; i++)
+            cout << nums[i] << " ";
+        cout << endl;
+
+        // 2- Rotate first K elements
+
+        left = 0;
+        right = k - 1;
+
+        while (left < right) {
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < size; i++)
+            cout << nums[i] << " ";
+        cout << endl;
+
+        // 3- Rotate last elements
+
+        left = k;
+        right = size - 1;
+
+        // cout << "left " << left << endl;
+        // cout << "right " << right << endl;
+
+        while(left<right){
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+};
