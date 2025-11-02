b57f4972b203a4cc49a9eaf5ee816d822eac0a0d
Salman Jan <jsalman862@gmail.com>
2025-11-02 15:44:59 +0500

    Time: 0 ms (100.00%) | Memory: 14.7 MB (51.46%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,15 +5,33 @@ public:
 
         vector<int> answer(2);
 
-        for(int i=0; i<n-1; i++){
-            for(int j= i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
-                }
+        // Brute force O(n^2)
+
+        // for(int i=0; i<n-1; i++){
+        //     for(int j= i+1; j<n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             answer[0] = i;
+        //             answer[1] = j;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // Sorting will distrub order of indices, so you'll need O(n) extra space plus O(nlogn) for sorting
+        // Using Hashset
+
+        unordered_map<int, int> seen;
+        for(int i=0; i<n; i++){
+            int toFind = target - nums[i];
+
+            if(seen.count(toFind) > 0){
+                answer[0] = i;
+                answer[1] = seen[toFind];
+                break;
             }
+            seen.insert({nums[i],i});
         }
+
 
         return answer;
     }
