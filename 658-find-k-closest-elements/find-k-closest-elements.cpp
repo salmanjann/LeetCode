7b3c9e377fcbb33798fdc4345be1219d4d395ca6
Salman Jan <jsalman862@gmail.com>
2025-11-05 11:00:51 +0500

    Time: 416 ms (5.27%) | Memory: 35.9 MB (79.55%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -4,30 +4,88 @@ public:
         vector<int> answer(k);
         int n = arr.size();
 
-        for(int i=0; i<k;i++)
-            answer[i] = arr[i];
+        int start = 0; 
+        int end = n-1;
+        int mid;
 
-        int kPtr = 0;
+        int lastStart= start,lastEnd= end;
+        int closest = -1;
 
-        for(int i=k; i<n;i++){
-            if(abs(answer[kPtr] - x) < abs(arr[i] - x)){
-                kPtr++;
+        while(start <= end){
+            if(start < end){
+                lastStart = start;
+                lastEnd = end;
             }
-            else if(abs(answer[kPtr] - x) > abs(arr[i] - x)){
-                answer[kPtr++] = arr[i];
-            }
-            else{
-                if(answer[kPtr] < arr[i])
-                    kPtr++;
-                else
-                    answer[kPtr++] = arr[i];
 
+            mid = (start + end) / 2;
+
+            if(arr[mid] == x){
+                closest = mid;
+                break;
             }
-            kPtr %= k;
+            else if(arr[mid] < x )
+                start = mid + 1;
+            else 
+                end = mid -1;
         }
 
-        sort(answer.begin(),answer.end());
+        cout<<"Start "<<start << " end "<<end<<endl;
+        
+        if(closest == -1){
+            if(end < 0)
+                closest = start;
+            else if(start > n-1)
+                closest = end;
+            else if(abs(arr[start] - x) < abs(arr[end] - x))
+                closest = start;
+            else if(abs(arr[start] - x) > abs(arr[end] - x))
+                closest = end;
+            else{
+                if(arr[start] < arr[end])
+                    closest = start;
+                else
+                    closest = end;
+            }
+        }
 
+        cout<<"closest " << closest<<endl;
+        // if(k==1)
+        // {
+        //     answer[0] = arr[closest];
+        //     return answer;
+        // }
+
+        int left = closest -1;
+        int right = closest + 1;
+
+        for(int i=0; i<k-1; i++){
+        cout<<"Left "<<left<<" Right "<<right<<endl; 
+            if(left < 0){
+                right++;
+            }
+            else if(right > n-1){
+                left--;
+            }
+            else if(abs(arr[left] - x) < abs(arr[right] - x)){
+                left--;
+            }
+            else if(abs(arr[left] - x) > abs(arr[right] - x)){
+                right++;
+            }
+            else{
+                if(left <= right)
+                    left--;
+                else
+                    right++;
+            }
+
+        }
+
+        cout<<"Left "<<left<<" Right "<<right<<endl; 
+        left++;
+        for(int i= 0; i<k; i++){
+            answer[i] = arr[left++];
+        }
         return answer;
     }
 };
