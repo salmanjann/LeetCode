3d224af5e1971218f8b188e0a12ec2a92b9d30f8
Salman Jan <jsalman862@gmail.com>
2025-11-06 12:32:00 +0500

    Time: 782 ms (5.72%) | Memory: 26.1 MB (49.95%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+
+        int sum = 0;
+        int reserveSum = 0;
+        int sumTillMax = 0;
+        int maxI = -1;
+        for(int left = 0; left<n-1; left++){
+            reserveSum = 0;
+            sumTillMax = 0;
+            maxI = left + 1;
+            // cout<<"Left "<<height[left] <<endl;
+            for(int right = left +1; right< n; right++){
+                // cout<<" right " <<height[right] <<endl;
+                if(height[right] < height[left]){
+                    if(height[right] > height[maxI]){
+                        sumTillMax = reserveSum;
+                        maxI = right;
+                    }
+                    reserveSum += height[left] - height[right];
+                    // cout<< " Res sum "<<reserveSum<<endl;
+
+                }
+                else {
+                    maxI = right;
+                    sum += reserveSum;
+                    left = right-1;
+                    break;
+                }
+            }
+            // cout<<"Maxi "<<height[maxI]<<endl;
+            // cout<<"Sum Till "<<sumTillMax<<endl;
+            if(height[maxI] < height[left]){
+                int diff = height[left] - height[maxI];
+                int elements = maxI - left -1;
+
+                if(elements>=1){
+                    sum += sumTillMax - (diff * elements);
+                    left = maxI -1; 
+                }
+            }
+        }
+
+        return sum;
+    }
+};
