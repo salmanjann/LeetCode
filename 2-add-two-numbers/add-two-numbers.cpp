ba297ea2206834b965d1f0cdfa87d86f23ba4a58
Salman Jan <jsalman862@gmail.com>
2025-11-11 11:00:41 +0500

    Time: 0 ms (100.00%) | Memory: 77.2 MB (13.58%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -12,72 +12,55 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        int sum = carry + l1->val + l2->val;
-        if (sum >= 10)
-            carry = 1;
-        else {
-            carry = 0;
-        }
+        int sum = 0;
 
-        ListNode* ans = new ListNode(sum % 10);
-        ListNode* ptr = ans;
+        ListNode* head = new ListNode();
+        ListNode* ptr = head;
+        sum = l1->val + l2->val;
+        head->val = sum % 10;
+        carry = sum > 9 ? 1 : 0;
 
-        l1 = l1->next;
+        l1= l1->next;
         l2 = l2->next;
 
-        while (l1 && l2) {
-            sum = carry + l1->val + l2->val;
-            if (sum >= 10)
-                carry = 1;
-            else {
-                carry = 0;
-            }
-
-            ListNode* newNode = new ListNode(sum % 10);
+        while(l1 && l2){
+            sum = l1->val + l2->val + carry;
+            ListNode* newNode = new ListNode(sum%10);
             ptr->next = newNode;
-            ptr = newNode;
+            ptr = ptr->next;
+            carry = sum > 9 ? 1 : 0;
 
-            l1 = l1->next;
+            l1= l1->next;
             l2 = l2->next;
         }
 
-        if (l1) {
-            while (l1) {
-                sum = carry + l1->val;
-                if (sum >= 10)
-                    carry = 1;
-                else {
-                    carry = 0;
-                }
+        while(l1){
+            sum = l1->val + carry;
+            ListNode* newNode = new ListNode(sum%10);
+            ptr->next = newNode;
+            ptr = ptr->next;
 
-                ListNode* newNode = new ListNode(sum % 10);
-                ptr->next = newNode;
-                ptr = newNode;
+            carry = sum > 9 ? 1 : 0;
 
-                l1 = l1->next;
-            }
-        } else if (l2) {
-            while (l2) {
-                sum = carry + l2->val;
-                if (sum >= 10)
-                    carry = 1;
-                else {
-                    carry = 0;
-                }
-
-                ListNode* newNode = new ListNode(sum % 10);
-                ptr->next = newNode;
-                ptr = newNode;
-
-                l2 = l2->next;
-            }
+            l1= l1->next;
         }
 
-        if (carry == 1) {
+        while(l2){
+            sum = l2->val + carry;
+            ListNode* newNode = new ListNode(sum%10);
+            ptr->next = newNode;
+            ptr = ptr->next;
+
+            carry = sum > 9 ? 1 : 0;
+
+            l2= l2->next;
+        }
+
+        if(carry){
             ListNode* newNode = new ListNode(1);
             ptr->next = newNode;
         }
 
-        return ans;
+        return head;
     }
 };
