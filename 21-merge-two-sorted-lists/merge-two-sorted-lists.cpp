5c86f702444c82dd113b8bc80d0e220844b37b6c
Salman Jan <jsalman862@gmail.com>
2025-11-12 13:08:03 +0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (27.68%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -16,47 +16,62 @@ public:
         if(!list2)
             return list1;
 
-        ListNode* head= NULL;
-        ListNode* ptr = head;
+        // Recurssion
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
 
-        ListNode* ptr1 = list1;
-        ListNode* ptr2 = list2;
+        return NULL;
         
-        int val = 0;
-        while(ptr1 && ptr2){
-            if(ptr1->val <= ptr2->val){
-                val = ptr1->val;
-                ptr1 = ptr1->next;
-            }
-            else{
-                val = ptr2->val;
-                ptr2 = ptr2->next;
-            }
+        
+        // Iterative
 
-            ListNode* newNode = new ListNode(val);
-            if(!head){
-                head = newNode;
-                ptr = head;
-            }
-            else{
-                ptr->next = newNode;
-                ptr = newNode;
-            }
-        }
-        while(ptr1){
-            ListNode* newNode = new ListNode(ptr1->val);
-            ptr->next = newNode;
-            ptr = newNode;
+        // ListNode* head= NULL;
+        // ListNode* ptr = head;
 
-            ptr1 = ptr1->next;
-        }
-        while(ptr2){
-            ListNode* newNode = new ListNode(ptr2->val);
-            ptr->next = newNode;
-            ptr = newNode;
-            ptr2 = ptr2->next;
+        // ListNode* ptr1 = list1;
+        // ListNode* ptr2 = list2;
+        
+        // int val = 0;
+        // while(ptr1 && ptr2){
+        //     if(ptr1->val <= ptr2->val){
+        //         val = ptr1->val;
+        //         ptr1 = ptr1->next;
+        //     }
+        //     else{
+        //         val = ptr2->val;
+        //         ptr2 = ptr2->next;
+        //     }
 
-        }
-        return head;
+        //     ListNode* newNode = new ListNode(val);
+        //     if(!head){
+        //         head = newNode;
+        //         ptr = head;
+        //     }
+        //     else{
+        //         ptr->next = newNode;
+        //         ptr = newNode;
+        //     }
+        // }
+        // while(ptr1){
+        //     ListNode* newNode = new ListNode(ptr1->val);
+        //     ptr->next = newNode;
+        //     ptr = newNode;
+
+        //     ptr1 = ptr1->next;
+        // }
+        // while(ptr2){
+        //     ListNode* newNode = new ListNode(ptr2->val);
+        //     ptr->next = newNode;
+        //     ptr = newNode;
+        //     ptr2 = ptr2->next;
+
+        // }
+        // return head;
     }
 };
